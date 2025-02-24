add548fe586e7f99de4947f733264f3c6d358f85
Z320 C001 <landadlh603@gmail.com>
2025-02-24 13:01:13 +0100

    Fix malas prácticas

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
-let pronoun = ['the', 'our'];
-let adj = ['great', 'big'];
-let noun = ['jogger', 'racoon'];
-let domain = ['.com', '.net', '.us', '.io'];
+let pronouns = ['the', 'our'];
+let adjetives = ['great', 'big'];
+let nouns = ['jogger', 'racoon'];
+let domains = ['.com', '.net', '.us', '.io'];
 
-for (let i = 0; i < pronoun.length; i++) {
-    for (let j = 0; j < adj.length; j++) {
-        for (let k = 0; k < noun.length; k++) {
-            for (let l = 0; l < domain.length; l++) {
-                console.log(pronoun[i] + adj[j] + noun[k] + domain[l]);
-            }
-        }
-    }
+let domainGenerator = () => {
+    pronouns.flatMap(pronoun => adjetives.flatMap(adjetives => nouns.flatMap(noun => domains.map(domain => console.log(pronoun + adjetives + noun + domain)))));
 }
+
+
+// for (let i = 0; i < pronoun.length; i++) {
+//     for (let j = 0; j < adj.length; j++) {
+//         for (let k = 0; k < noun.length; k++) {
+//             for (let l = 0; l < domain.length; l++) {
+//                 console.log(pronoun[i] + adj[j] + noun[k] + domain[l]);
+//             }
+//         }
+//     }
+// }
 
 
